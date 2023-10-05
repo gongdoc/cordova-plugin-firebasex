e1192ccfbc02ee367f6c81ef2966e40c1cb61e63
progeddie <prog.eddie@gmail.com>
2023-10-05 16:07:59 +0900

    Revert "오류수정"
    This reverts commit bb66c099b49cd9b323e5c1564b4584b125d8c8ba.

--- scripts/after_prepare.js
+++ scripts/after_prepare.js
@@ -9,22 +9,6 @@
  */
 var fs = require('fs');
 var path = require("path");
-
-/**** ****/
-let dirPath = path.resolve('platforms/android/app/src/main/AndroidManifest.xml');
-
-let manifest = fs.readFileSync(dirPath, {
-    encoding: 'utf-8'
-});
-
-// Strips ALL occurrences of <uses-permission android:name="androoid.permission.WRITE_EXTERNAL_STORAGE" />
-// If you have several conflicts (of different maxSDKVersion, or in different formats) then the regex
-// may need to be adjusted, or repeated for each format.
-manifest = manifest.replace(/^(\s)+<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" \/>$/gm, '');
-
-fs.writeFileSync(dirPath, manifest);
-/**** ****/
-
 var execSync = require('child_process').execSync;
 var utilities = require("./lib/utilities");
 
@@ -171,6 +155,4 @@ module.exports = function(context){
             });
         }
     }
-
-
 };

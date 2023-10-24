4b75ffc21d48c5ef553316259abe6225442c347f
‘prog.eddie@gmail.com’ <prog.eddie@gmail.com>
2023-10-24 15:47:50 +0900

    오류수정

--- src/android/FirebasePluginMessagingService.java
+++ src/android/FirebasePluginMessagingService.java
@@ -286,7 +286,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
 
                 boolean showNotification = (FirebasePlugin.inBackground() || !FirebasePlugin.hasNotificationsCallback()) && (!TextUtils.isEmpty(text) || !TextUtils.isEmpty(title));
                 vibrate = "500, 200, 500";
-                sendMessage(id, title, text, data, showNotification, lights, vibrate, color, messageType);
+                sendMessage(id, title, text, data, showNotification, lights, vibrate, color, messageType, icon);
 
                 PushWakeLock.releaseWakeLock();
             }
@@ -321,7 +321,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
         }
     }
                             
-    private void sendMessage(String id, String title, String messageBody, Map<String, String> data, boolean showNotification, String lights, String vibrate, String color, String messageType) {
+    private void sendMessage(String id, String title, String messageBody, Map<String, String> data, boolean showNotification, String lights, String vibrate, String color, String messageType, String icon) {
         Bundle bundle = new Bundle();
         for (String key : data.keySet()) {
             bundle.putString(key, data.get(key));

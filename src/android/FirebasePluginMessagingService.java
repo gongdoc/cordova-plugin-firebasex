128aa3cb90d8dcc90d1d46829dc86e8ceb41c6b6
‘prog.eddie@gmail.com’ <prog.eddie@gmail.com>
2023-10-30 11:23:02 +0900

    푸시테스트

--- src/android/FirebasePluginMessagingService.java
+++ src/android/FirebasePluginMessagingService.java
@@ -465,7 +465,39 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
             // Display notification
             NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             Log.d(TAG, "show notification: "+notification.toString());
-            notificationManager.notify(id.hashCode(), notification);
+
+            if (notificationManager != null) {
+                // Since android Oreo notification channel is needed.
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+
+                    AudioAttributes attributes = new AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                            .build();
+                    if (sound != null) {
+                        channel.setSound(soundPath, attributes);
+                    } else {
+                        Uri uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                        channel.setSound(uri, attributes);
+                    }
+
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+                if (android.os.Build.VERSION.SDK_INT >= 26) {
+                    if (audioManager != null) {
+                        int ringerMode = audioManager.getRingerMode();
+                        if (ringerMode == AudioManager.RINGER_MODE_VIBRATE) {
+                            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+                            if (channel.shouldVibrate()) {
+                                channel.setVibrationPattern(defaultVibration);
+                            }
+                        }
+                    }
+                }
+
+                notificationManager.notify(id.hashCode(), notification);
+            }
         } else {
             bundle.putBoolean("tap", false);
             bundle.putString("title", title);

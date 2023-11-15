fb6ab2c08afe02aa51aca3aff4d295079b664a34
‘prog.eddie@gmail.com’ <prog.eddie@gmail.com>
2023-11-15 13:50:50 +0900

    11.15.2 테스트

--- src/android/FirebasePluginMessagingService.java
+++ src/android/FirebasePluginMessagingService.java
@@ -154,6 +154,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
             String light = null;
             String color = null;
             String icon = null;
+            String channelId = null;
             String visibility = null;
             String priority = null;
             String image = null;
@@ -285,7 +286,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
                 PushWakeLock.acquireWakeLock(getApplicationContext());
 
                 boolean showNotification = (FirebasePlugin.inBackground() || !FirebasePlugin.hasNotificationsCallback()) && (!TextUtils.isEmpty(text) || !TextUtils.isEmpty(title));
-                sendMessage(id, title, text, data, showNotification, lights, vibrate, color, sound);
+                sendMessage(id, title, text, data, showNotification, lights, vibrate, color, sound, channelId);
                    
                 PushWakeLock.releaseWakeLock();
             }
@@ -302,6 +303,55 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
                         bundle.putString(entry.getKey(), entry.getValue());
                     }
                     intent.putExtras(bundle);
+
+
+                    if (flagPush.equals("N")) {
+                    try {
+                        final AudioManager audioManager = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+                        if (audioManager != null) {
+                            int ringerMode = audioManager.getRingerMode();
+                            if (ringerMode == AudioManager.RINGER_MODE_NORMAL) {
+                                Uri soundPath = RingtoneManager.getActualDefaultRingtoneUri(context, RingtoneManager.TYPE_NOTIFICATION);
+                                if (sound != null) {
+                                    soundPath = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getPackageName() + "/raw/" + sound);
+                                }
+
+                                final int maxVolumeMusic = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                                final int volumeMusic = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+                                int maxVolumeNotification = audioManager.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION);
+                                int volumeNotification = audioManager.getStreamVolume(AudioManager.STREAM_NOTIFICATION);
+
+                                int volume = volumeNotification * maxVolumeMusic / maxVolumeNotification;
+                                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+
+                                final MediaPlayer mediaPlayer = new MediaPlayer();
+                                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                                mediaPlayer.setDataSource(getApplicationContext(), soundPath);
+                                mediaPlayer.prepare();
+                                mediaPlayer.start();
+                                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                                    public void onCompletion(MediaPlayer mp) {
+                                        mediaPlayer.release();
+                                        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volumeMusic, 0);
+                                    }
+                                });
+                            }
+
+                            if (ringerMode == AudioManager.RINGER_MODE_VIBRATE) {
+                                long[] defaultVibration = new long[] { 0, 280, 250, 280, 250 };
+                                Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                                if (vibrator != null && vibrator.hasVibrator()) {
+                                    if (android.os.Build.VERSION.SDK_INT >= 26) {
+                                        vibrator.vibrate(VibrationEffect.createWaveform(defaultVibration, -1));
+                                    } else {
+                                        vibrator.vibrate(defaultVibration, -1);
+                                    }
+                                }
+                            }
+                        }
+                    } catch (Exception ex) {
+                        Log.d(TAG, "Sound file load failed");
+                    }
 
                     startActivity(intent);
 
@@ -320,7 +370,7 @@ public class FirebasePluginMessagingService extends FirebaseMessagingService {
         }
     }
                             
-    private void sendMessage(String id, String title, String messageBody, Map<String, String> data, boolean showNotification, String lights, String vibrate, String color, String sound) {
+    private void sendMessage(String id, String title, String messageBody, Map<String, String> data, boolean showNotification, String lights, String vibrate, String color, String sound, String channelId) {
         Bundle bundle = new Bundle();
         for (String key : data.keySet()) {
             bundle.putString(key, data.get(key));
